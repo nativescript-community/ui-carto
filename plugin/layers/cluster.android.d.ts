54ded61a28a22028e32a6aa6df90123ec8f10c5a
Martin Guillon <martin.guillon@akylas.fr>
2018-10-23 15:12:32 +0200

    cluster

---
+++ plugin/layers/cluster.android.d.ts
@@ -0,0 +1,13 @@
+import { BaseNative } from '../carto.common';
+import { ClusterElementBuilderOptions } from './cluster';
+import { MapPos } from '../core/core';
+import { VectorElementVector } from '../vectorelements/vectorelements';
+import { BaseVectorElement } from '../vectorelements/vectorelements.android';
+export interface ClusterElementBuilderNative extends com.akylas.carto.additions.AKClusterElementBuilder {
+    new (owner: WeakRef<ClusterElementBuilder>): ClusterElementBuilderNative;
+}
+export declare class ClusterElementBuilder extends BaseNative<com.akylas.carto.additions.AKClusterElementBuilder, ClusterElementBuilderOptions> {
+    bitmap: string;
+    createNative(): ClusterElementBuilderNative;
+    buildClusterElement?: (pos: MapPos, elements: VectorElementVector) => BaseVectorElement<any, any> | com.carto.vectorelements.VectorElement;
+}
