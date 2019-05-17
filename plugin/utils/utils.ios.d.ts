26a8055eb96f55a739159e5dc9cd2574146eb350
Martin Guillon <martin.guillon@akylas.fr>
2019-05-17 22:46:26 +0200

    chore: cleanup

--- plugin/utils/utils.ios.d.ts
+++
@@ -1,9 +0,0 @@
-import { BaseNative } from '../carto.common';
-import { ZippedAssetPackageOptions } from './utils';
-export declare function nativeVectorToArray(nVector: com.carto.core.StringVector): any[];
-export declare function nativeVariantToJS(variant: NTVariant): any;
-export declare function nativeMapToJS(theMap: NTStringVariantMap): {};
-export declare function setShowDebug(value: boolean): void;
-export declare class ZippedAssetPackage extends BaseNative<NTZippedAssetPackage, ZippedAssetPackageOptions> {
-    createNative(options: ZippedAssetPackageOptions): NTZippedAssetPackage;
-}
