3f5e3567eef2f60fa5ee3dad2ec5d054a165ef7c
Martin Guillon <martin.guillon@akylas.fr>
2019-05-27 09:56:23 +0200

    chore: should be commited

--- plugin/search/search.ios.d.ts
+++
@@ -1,9 +0,0 @@
-import { BaseNative } from '../carto.common';
-import { VectorTileFeatureCollection } from '../geometry/feature';
-import { SearchRequest, VectorTileSearchServiceOptions } from './search';
-export declare class VectorTileSearchService extends BaseNative<NTVectorTileSearchService, VectorTileSearchServiceOptions> {
-    minZoom: number;
-    maxZoom: number;
-    createNative(options: VectorTileSearchServiceOptions): NTVectorTileSearchService;
-    findFeatures(options: SearchRequest): VectorTileFeatureCollection;
-}
