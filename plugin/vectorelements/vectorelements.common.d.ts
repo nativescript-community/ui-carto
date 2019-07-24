5ad42e283d904b0e67f076adc7c1c0affa766ca4
Martin Guillon <martin.guillon@akylas.fr>
2019-07-24 14:30:08 +0200

    chore: cleanup

--- plugin/vectorelements/vectorelements.common.d.ts
+++
@@ -1,4 +0,0 @@
-import { BaseNative } from '../carto.common';
-import { VectorElementStyleBuilderOptions } from './vectorelements';
-export declare abstract class BaseVectorElementStyleBuilder<T, U extends VectorElementStyleBuilderOptions> extends BaseNative<T, U> {
-}
