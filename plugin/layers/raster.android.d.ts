ba51fb1d7d0bc41b56c4d9557656b2ff53df260b
Martin Guillon <martin.guillon@akylas.fr>
2020-05-26 17:16:01 +0200

    chore: cleanup

--- plugin/layers/raster.android.d.ts
+++
@@ -1,8 +0,0 @@
-import { CartoOnlineRasterTileLayerOptions, RasterTileLayerOptions } from './raster';
-import { RasterTileLayerBase } from './raster.common';
-export declare class RasterTileLayer extends RasterTileLayerBase<com.carto.layers.RasterTileLayer, RasterTileLayerOptions> {
-    createNative(options: RasterTileLayerOptions): com.carto.layers.RasterTileLayer;
-}
-export declare class CartoOnlineRasterTileLayer extends RasterTileLayerBase<com.carto.layers.CartoOnlineRasterTileLayer, CartoOnlineRasterTileLayerOptions> {
-    createNative(options: any): com.carto.layers.CartoOnlineRasterTileLayer;
-}
