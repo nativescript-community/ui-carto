8a047e43d00721292657ce39a93ccf7bc57d1482
Martin Guillon <martin.guillon@akylas.fr>
2020-05-26 17:15:26 +0200

    feat: added HillshadeRasterTileLayer on android

--- src/layers/raster.android.ts
+++ src/layers/raster.android.ts
@@ -1,5 +1,6 @@
-import { CartoOnlineRasterTileLayerOptions, RasterTileLayerOptions } from './raster';
+import { CartoOnlineRasterTileLayerOptions, HillshadeRasterTileLayerOptions, RasterTileLayerOptions } from './raster';
 import { RasterTileLayerBase } from './raster.common';
+import { nativeProperty } from 'nativescript-carto';
 
 export class RasterTileLayer extends RasterTileLayerBase<com.carto.layers.RasterTileLayer, RasterTileLayerOptions> {
     createNative(options: RasterTileLayerOptions) {
@@ -10,5 +11,13 @@ export class RasterTileLayer extends RasterTileLayerBase<com.carto.layers.Raster
 export class CartoOnlineRasterTileLayer extends RasterTileLayerBase<com.carto.layers.CartoOnlineRasterTileLayer, CartoOnlineRasterTileLayerOptions> {
     createNative(options) {
         return new com.carto.layers.CartoOnlineRasterTileLayer(options.source);
+    }
+}
+
+export class HillshadeRasterTileLayer extends RasterTileLayerBase<com.carto.layers.RasterTileLayer, HillshadeRasterTileLayerOptions> {
+    @nativeProperty heightScale: number;
+    @nativeProperty contrast: number;
+    createNative(options) {
+        return new (com.carto.layers as any).HillshadeRasterTileLayer(options.dataSource.getNative());
     }
 }
--- src/layers/raster.d.ts
+++ src/layers/raster.d.ts
@@ -4,10 +4,20 @@ import { TileDataSource } from '../datasources';
 export interface RasterTileLayerOptions extends TileLayerOptions {
     dataSource?: TileDataSource<any, any>;
 }
-export interface CartoOnlineRasterTileLayerOptions extends RasterTileLayerOptions {
-    source?: string;
-}
 export class RasterTileLayer extends TileLayer<any, RasterTileLayerOptions> {
     dataSource?: TileDataSource<any, any>;
 }
+
+export interface CartoOnlineRasterTileLayerOptions extends RasterTileLayerOptions {
+    source?: string;
+}
 export class CartoOnlineRasterTileLayer extends TileLayer<any, CartoOnlineRasterTileLayerOptions> {}
+
+export interface HillshadeRasterTileLayerOptions extends RasterTileLayerOptions {
+    heightScale?: number;
+    contrast?: number;
+}
+export class HillshadeRasterTileLayer extends TileLayer<any, HillshadeRasterTileLayerOptions> {
+    heightScale?: number;
+    contrast?: number;
+}
