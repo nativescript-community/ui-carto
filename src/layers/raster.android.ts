7608ebbf836dee273a7323a41ff8c9d7ad8f38a9
Martin Guillon <martin.guillon@akylas.fr>
2021-04-16 15:48:46 +0200

    chore: android raster click
    # Conflicts:
    #	src/layers/raster.android.ts

--- src/layers/raster.android.ts
+++ src/layers/raster.android.ts
@@ -1,8 +1,15 @@
-import { CartoOnlineRasterTileLayerOptions, HillshadeRasterTileLayerOptions, RasterTileFilterMode as IRasterTileFilterMode, RasterTileLayerOptions } from './raster';
+import {
+    CartoOnlineRasterTileLayerOptions,
+    HillshadeRasterTileLayerOptions,
+    RasterTileEventListener as IRasterTileEventListener,
+    RasterTileFilterMode as IRasterTileFilterMode,
+    RasterTileLayerOptions,
+} from './raster';
 import { RasterTileLayerBase } from './raster.common';
 import { mapPosVectorFromArgs, nativeAndroidEnumProperty, nativeColorProperty, nativeProperty } from '../';
 import { Color } from '@nativescript/core/color';
-import { IntVector, MapPos, MapPosVector, toNativeMapPos } from '../core';
+import { IntVector, MapPos, MapPosVector, MapVec, fromNativeMapPos, toNativeMapPos } from '../core';
+import { Projection } from '../projections';
 
 export const RasterTileFilterMode = {
     get RASTER_TILE_FILTER_MODE_NEAREST() {
@@ -16,8 +23,60 @@ export const RasterTileFilterMode = {
     },
 };
 
-export class RasterTileLayer extends RasterTileLayerBase<com.carto.layers.RasterTileLayer, RasterTileLayerOptions> {
-    @nativeAndroidEnumProperty(com.carto.layers.RasterTileFilterMode, {}) tileFilterMode: IRasterTileFilterMode;
+export abstract class RasterTileLayerCommon<NativeClass extends com.carto.layers.RasterTileLayer, U extends RasterTileLayerOptions> extends RasterTileLayerBase<NativeClass, U> {
+    projection?: Projection;
+    clickListener?: IRasterTileEventListener;
+    nClickListener?: com.akylas.carto.additions.AKRasterTileEventListener;
+    constructor(options) {
+        super(options);
+        for (const property of ['elementListener', 'nElementListener']) {
+            const descriptor = Object.getOwnPropertyDescriptor(RasterTileLayer.prototype, property);
+            if (descriptor) {
+                descriptor.enumerable = false;
+            }
+        }
+    }
+    setRasterTileEventListener(listener: IRasterTileEventListener, projection?: Projection) {
+        this.clickListener = listener;
+        this.projection = projection;
+        if (listener) {
+            if (!this.nClickListener) {
+                this.nClickListener = new com.akylas.carto.additions.AKRasterTileEventListener(
+                    new com.akylas.carto.additions.AKRasterTileEventListener.Listener({
+                        onRasterTileClicked: this.onRasterTileClicked.bind(this),
+                    })
+                );
+            }
+            this.getNative().setRasterTileEventListener(this.nClickListener);
+        } else {
+            this.nClickListener = null;
+            this.getNative().setRasterTileEventListener(null);
+        }
+    }
+    onRasterTileClicked(info: com.carto.ui.RasterTileClickInfo) {
+        if (this.clickListener && this.clickListener.onRasterTileClicked) {
+            let position = info.getClickPos();
+            if (this.projection) {
+                const layerProj = this.getNative().getDataSource().getProjection();
+                const nProj = this.projection.getNative();
+                position = nProj.fromWgs84(layerProj.toWgs84(position));
+            }
+            return (
+                this.clickListener.onRasterTileClicked.call(this.clickListener, {
+                    clickType: info.getClickType().swigValue(),
+                    layer: this,
+                    nearestColor: new Color(info.getNearestColor().getARGB()),
+                    interpolatedColor: new Color(info.getInterpolatedColor().getARGB()),
+                    position: fromNativeMapPos(position),
+                }) || false
+            );
+        }
+        return false;
+    }
+}
+
+export class RasterTileLayer extends RasterTileLayerCommon<com.carto.layers.RasterTileLayer, RasterTileLayerOptions> {
+    @nativeProperty tileFilterMode: IRasterTileFilterMode;
     createNative(options: RasterTileLayerOptions) {
         return new com.carto.layers.RasterTileLayer(options.dataSource.getNative());
     }
@@ -29,7 +88,7 @@ export class CartoOnlineRasterTileLayer extends RasterTileLayerBase<com.carto.la
     }
 }
 
-export class HillshadeRasterTileLayer extends RasterTileLayerBase<com.akylas.carto.additions.AKHillshadeRasterTileLayer, HillshadeRasterTileLayerOptions> {
+export class HillshadeRasterTileLayer extends RasterTileLayerCommon<com.akylas.carto.additions.AKHillshadeRasterTileLayer, HillshadeRasterTileLayerOptions> {
     @nativeProperty heightScale: number;
     @nativeProperty contrast: number;
     @nativeProperty illuminationDirection: number;
