46fdb024c64f2a72490837d13e594f26e2c94a10
Martin Guillon <martin.guillon@akylas.fr>
2021-08-16 10:53:46 +0200

    test: demo update

--- demo_vue/app.webpack.config.js
+++ demo_vue/app.webpack.config.js
@@ -7,13 +7,13 @@ module.exports = (env, params = {}) => {
     } = env;
 
     const config = webpackConfig(env, params);
-    const coreModulesPackageName = fork ? '@akylas/nativescript' : '@nativescript/core';
-    config.resolve.modules = [resolve(__dirname, `node_modules/${coreModulesPackageName}`), resolve(__dirname, 'node_modules'), `node_modules/${coreModulesPackageName}`, 'node_modules'];
-    Object.assign(config.resolve.alias, {
-        '@nativescript/core': `${coreModulesPackageName}`,
-        'tns-core-modules': `${coreModulesPackageName}`
-    });
+    // const coreModulesPackageName = fork ? '@akylas/nativescript' : '@nativescript/core';
+    // config.resolve.modules = [resolve(__dirname, `node_modules/${coreModulesPackageName}`), resolve(__dirname, 'node_modules'), `node_modules/${coreModulesPackageName}`, 'node_modules'];
+    // Object.assign(config.resolve.alias, {
+    //     '@nativescript/core': `${coreModulesPackageName}`,
+    //     'tns-core-modules': `${coreModulesPackageName}`
+    // });
 
-    console.log('coreModulesPackageName', coreModulesPackageName);
+    // console.log('coreModulesPackageName', coreModulesPackageName);
     return config;
 };
