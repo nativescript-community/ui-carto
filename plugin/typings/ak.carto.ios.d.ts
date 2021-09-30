3390e334b6e3a4f02033a15365a9b15101a3955b
Martin Guillon <martin.guillon@akylas.fr>
2021-09-30 16:06:42 +0200

    chore: ios typings

--- plugin/typings/ak.carto.ios.d.ts
+++ plugin/typings/ak.carto.ios.d.ts
@@ -5,3 +5,19 @@
 //     constructor(o?: { minZoom: number; maxZoom: number; baseURL: string });
 //     setAutoHD(value: boolean);
 // }
+
+declare class CartoAdditionsUtils extends NSObject {
+    static alloc(): CartoAdditionsUtils; // inherited from NSObject
+
+    static distanceToEndWithIntPoly(index: number, poly: NTMapPosVector): number;
+
+    static isLocationOnPoly(point: NTMapPos, poly: NTMapPosVector): number;
+
+    static isLocationOnPolyClosed(point: NTMapPos, poly: NTMapPosVector, closed: boolean): number;
+
+    static isLocationOnPolyClosedGeodesic(point: NTMapPos, poly: NTMapPosVector, closed: boolean, geodesic: boolean): number;
+
+    static isLocationOnPolyClosedGeodesicToleranceEarth(point: NTMapPos, poly: NTMapPosVector, closed: boolean, geodesic: boolean, toleranceEarth: number): number;
+
+    static new(): CartoAdditionsUtils; // inherited from NSObject
+}
