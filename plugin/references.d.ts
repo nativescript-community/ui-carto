c96dbefe44c8deb42c42bd1dcda97a51dcca4e7c
Martin Guillon <martin.guillon@akylas.fr>
2021-11-16 17:45:11 +0100

    chore: typings

--- plugin/references.d.ts
+++ plugin/references.d.ts
@@ -2,3 +2,4 @@
 /// <reference path="./typings/carto.ios.d.ts" />
 /// <reference path="./typings/ak.carto.ios.d.ts" />
 /// <reference path="./typings/ak.carto.android.d.ts" />
+/// <reference path="../node_modules/@nativescript/core/global-types.d.ts" />
