2af97149a9185c4debf6e36bc512cc4562dc8052
Martin Guillon <martin.guillon@akylas.fr>
2022-01-20 13:23:08 +0100

    fix: `reloadStyle` and missing `MBVectorTileDecoder`

--- src/ui-carto/vectortiles/index.d.ts
+++ src/ui-carto/vectortiles/index.d.ts
@@ -17,7 +17,27 @@ export class MBVectorTileDecoder extends BaseNative<any, MBVectorTileDecoderOpti
     style?: string;
     liveReload?: boolean;
     constructor(options: MBVectorTileDecoderOptions, native?: any);
+    reloadStyle();
     setStyleParameter(param: string, value: string);
+    setCartoCSSStyleSet(param0: string): void;
+
+    setCompiledStyleSet(param0: any): void;
+    getCompiledStyleSet(): any;
+
+    getCartoCSSStyleSet(): any;
+    getStyleParameter(param0: string): string;
+    getStyleParameters(): core.StringVector;
+    addFallbackFont(param0: core.BinaryData): void;
+    getMaxZoom(): number;
+    getMinZoom(): number;
+
+    // setFeatureIdOverride(value: boolean);
+    // isFeatureIdOverride(): boolean;
+    // setCartoCSSLayerNamesIgnored(ignore: boolean);
+    // isCartoCSSLayerNamesIgnored(): boolean;
+
+    // setLayerNameOverride(name: string);
+    // getLayerNameOverride(): string;
 }
 
 // export interface CartoVectorTileDecoderOptions extends VectorTileDecoderOptions {
