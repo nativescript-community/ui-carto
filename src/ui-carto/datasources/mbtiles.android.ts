d748bcf3e9531c1165dbbc8de185494a53977b97
Martin Guillon <martin.guillon@akylas.fr>
2022-01-20 13:23:20 +0100

    fix: scheme

--- src/ui-carto/datasources/mbtiles.android.ts
+++ src/ui-carto/datasources/mbtiles.android.ts
@@ -1,10 +1,24 @@
 import { MBTilesTileDataSourceOptions } from './mbtiles';
 import { TileDataSource } from '.';
 
+export const MBTilesScheme = {
+    get MBTILES_SCHEME_TMS() {
+        return com.carto.datasources.MBTilesScheme.MBTILES_SCHEME_TMS;
+    },
+    get MBTILES_SCHEME_XYZ() {
+        return com.carto.datasources.MBTilesScheme.MBTILES_SCHEME_XYZ;
+    }
+};
+
 export class MBTilesTileDataSource extends TileDataSource<com.carto.datasources.MBTilesTileDataSource, MBTilesTileDataSourceOptions> {
     createNative(options: MBTilesTileDataSourceOptions) {
         if (options.hasOwnProperty('minZoom') || options.hasOwnProperty('maxZoom')) {
-            return new com.carto.datasources.MBTilesTileDataSource(options.minZoom, options.maxZoom, options.databasePath);
+            return new com.carto.datasources.MBTilesTileDataSource(
+                options.minZoom || 0,
+                options.maxZoom || 24,
+                options.databasePath,
+                options.scheme || (com.carto.datasources.MBTilesScheme.MBTILES_SCHEME_TMS as any)
+            );
         } else {
             return new com.carto.datasources.MBTilesTileDataSource(options.databasePath);
         }
