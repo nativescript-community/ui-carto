6643e78b1d30c5f8df6550a8f9de12e4b9bed85e
Martin Guillon <martin.guillon@akylas.fr>
2022-01-20 13:21:21 +0100

    fix: scheme support for `MBTilesTileDataSource`

--- src/ui-carto/datasources/mbtiles.ios.ts
+++ src/ui-carto/datasources/mbtiles.ios.ts
@@ -3,7 +3,14 @@ import { TileDataSource } from '.';
 
 export class MBTilesTileDataSource extends TileDataSource<NTMBTilesTileDataSource, MBTilesTileDataSourceOptions> {
     createNative(options: MBTilesTileDataSourceOptions) {
-        return NTMBTilesTileDataSource.alloc().initWithPath(options.databasePath);
-        // return NTMBTilesTileDataSource.alloc().initWithMinZoomMaxZoomPath(options.minZoom, options.maxZoom, options.databasePath);
+        if (options.hasOwnProperty('minZoom') || options.hasOwnProperty('maxZoom')) {
+            return NTMBTilesTileDataSource.alloc().initWithMinZoomMaxZoomPath(
+                options.minZoom || 0,
+                options.maxZoom || 24,
+                (options.databasePath, options.scheme || NTMBTilesScheme.T_MBTILES_SCHEME_TMS) as any
+            );
+        } else {
+            return NTMBTilesTileDataSource.alloc().initWithPath(options.databasePath);
+        }
     }
 }
