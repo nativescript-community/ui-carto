1b5fcb336018e6482ecab8a91584f9d03ee5ecbd
Martin Guillon <martin.guillon@akylas.fr>
2022-01-20 13:21:21 +0100

    fix: scheme support for `MBTilesTileDataSource`

--- src/ui-carto/datasources/mbtiles.d.ts
+++ src/ui-carto/datasources/mbtiles.d.ts
@@ -1,6 +1,12 @@
 import { DataSource, TileDataSource, TileDataSourceOptions } from '.';
 
+export enum MBTilesScheme {
+    MBTILES_SCHEME_TMS,
+    MBTILES_SCHEME_XYZ
+}
+
 export interface MBTilesTileDataSourceOptions extends TileDataSourceOptions {
     databasePath?: string;
+    scheme?: MBTilesScheme;
 }
 export class MBTilesTileDataSource extends TileDataSource<any, MBTilesTileDataSourceOptions> {}
