3fb335716ab8e3376f003580ddcf8c9bdc6ffe68
Martin Guillon <martin.guillon@akylas.fr>
2022-02-16 11:27:01 +0100

    chore: demo snippets cleanup

--- demo-snippets/vue/install.ts
+++ demo-snippets/vue/install.ts
@@ -1,11 +1,9 @@
 import Vue from 'nativescript-vue';
 import Pager from '@nativescript-community/ui-carto/vue';
-import PersistentBottomSheetPlugin from '@nativescript-community/ui-persistent-bottomsheet/vue';
 
 import BasicRaster from './BasicRaster.vue';
 export function installPlugin() {
     Vue.use(Pager);
-    Vue.use(PersistentBottomSheetPlugin);
 }
 
 export const demos = [{ name: 'Basic Raster', path: 'basicraster', component: BasicRaster }];
