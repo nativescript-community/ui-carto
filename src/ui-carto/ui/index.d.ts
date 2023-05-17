44c0cceed8bf4ca2c5804ad82542446ce8115029
Martin Guillon <dev@akylas.fr>
2023-05-17 13:47:28 +0200

    chore: tpings

--- src/ui-carto/ui/index.d.ts
+++ src/ui-carto/ui/index.d.ts
@@ -133,6 +133,7 @@ export class MapOptions {
     setKineticPan(enabled: boolean): void;
     setKineticRotation(enabled: boolean): void;
     setKineticZoom(enabled: boolean): void;
+    setRotationGestures(enabled: boolean): void;
     /**
      * @param color  native Carto Color
      */
