e3bb8b2d2d840426ae80ddd024c77a4f747ac755
Martin Guillon <dev@akylas.fr>
2025-10-15 18:27:23 +0200

    feat: using latest sdk and available native SDK on iOS and Android

---
+++ packages/ui-carto/nativescript.config.ts
@@ -0,0 +1,12 @@
+export default {
+  ios: {
+    SPMPackages: [
+      {
+        name: 'carto-mobile-sdk',
+        libs: ['CartoMobileSDK'],
+        repositoryURL: 'https://github.com/Akylas/mobile-sdk-ios-swift.git',
+        version: '5.0.0',
+      },
+    ],
+  },
+}
