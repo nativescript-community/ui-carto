1a8b0dcc69efebf8ccc3394352a43725c2048a44
farfromrefuge <>
2026-03-16 11:29:12 +0100

    fix(ios): ios build fix by using SPM

--- packages/ui-carto/nativescript.config.ts
+++ packages/ui-carto/nativescript.config.ts
@@ -7,6 +7,12 @@ export default {
         repositoryURL: 'https://github.com/Akylas/mobile-sdk-ios-swift.git',
         version: '5.0.1',
       },
+      {
+        name: 'carto-SwiftTryCatch-sdk',
+        libs: ['SwiftTryCatch'],
+        repositoryURL: 'https://github.com/farfromrefug/SwiftTryCatch.git',
+        version: '1.0.0',
+      },
     ],
   },
 }
