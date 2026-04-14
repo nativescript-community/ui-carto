0cd8d3aacb0c375e60a0a8b6312be5927213068a
farfromrefuge <>
2026-04-14 11:22:25 +0200

    fix(ui-carto): android build fix

--- packages/ui-carto/platforms/android/java/com/akylas/carto/additions2/ServerPackagesCallback.java
+++
@@ -1,9 +0,0 @@
-package com.akylas.carto.additions2;
-
-import com.carto.packagemanager.PackageInfo;
-import com.carto.packagemanager.PackageInfoVector;
-
-public interface ServerPackagesCallback {
-
-    void onServerPackages(PackageInfoVector packages);
-}
